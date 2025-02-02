d9dbfff78c3f16e5abc1c40427bccf60ab7550ef
Harshada Shashank Yadav <137686756+HarshadaSYadav@users.noreply.github.com>
2025-02-02 23:02:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*", 
+              methods: ["GET", "POST", "PUT", "DELETE"],
+              allowedHeaders: ["Content-Type"] }));
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected"))
